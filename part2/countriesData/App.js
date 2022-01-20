ed6bf719ece0aa811ddfa5bf6c5728cd88819871
Vivz <73238880+VivCroft12@users.noreply.github.com>
2022-01-20 22:01:17 +0530

    Add files via upload

---
+++ part2/countriesData/App.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Display from './components/display'
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+    const [filteredNames, setFilteredNames] = useState([])
+    
+  const hook = () => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log("promise")
+        console.log(response.data.names)
+        setCountries(response.data)
+      })
+  }
+    
+  useEffect(hook, [])
+  console.log(countries)
+  const handleChange = (event) => {
+    setFilter(event.target.value)
+  }
+    
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const arr = countries.filter((country) => country.name.common.toUpperCase().includes(filter.toUpperCase()))
+    setFilteredNames(arr)
+    setFilter('')
+    console.log(arr)
+  }
+    
+  return (
+    <div className="App">
+      <form onSubmit = {handleSubmit}>
+        find countries <input onChange = {handleChange} value = {filter} />
+        <Display filteredNames = {filteredNames} />
+      </form>
+    </div>
+  );
+}
+
+export default App;
